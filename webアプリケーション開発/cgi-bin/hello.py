3d1019b03477709fb576cab9b2d77d47536b8d51
sungyo4869 <hchengjian1@gmail.com>
2024-01-01 01:46:54 +0900

    ランダムにサイコロの目を返すことができた

--- webアプリケーション開発/cgi-bin/hello.py
+++ webアプリケーション開発/cgi-bin/hello.py
@@ -1,16 +1,17 @@
 import sys
 import io
-
-from datetime import datetime
+import random
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+dice = random.randint(1, 6)
 
 print('Content-type: text/html; charset=UTF-8\r\n')
 print("<head>")
 print('<meta http-equiv="Content-type" content ="text/html; charset=UTF-8">')
-print("<title>ただいまの日時</title>")
+print("<title>サイコロ</title>")
 print("</head>")
 print("<body>")
-print("<p>ただいまの日時 : %s</p>"%datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+print("<p>サイコロの目は%dでした</p>"% dice)
 print("</body>")
 print("</html>")

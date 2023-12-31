9fd2850031538a9f09fdffcb5240f79d7ac07d19
sungyo4869 <hchengjian1@gmail.com>
2023-12-31 23:06:40 +0900

    要らない出力をけした

--- ネットワークプログラム/client.py
+++ ネットワークプログラム/client.py
@@ -7,14 +7,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバーを指定
     s.connect((ipaddr, port))
     # サーバーにメッセージを送る
-    print(sys.argv[1])
     s.send(str.encode(sys.argv[1]))
     # ネットワークのバッファサイズは1024
     # サーバーから文字列を取得する
-    print("送ったよ")
     
     data = s.recv(1024)
-    
-    print("受け取ったよ")
     
     print(data)
